be47cc05597bb12e63c535bec0a2a56df6a3ceef
Andre Soares <andregsilv@gmail.com>
2019-01-23 09:22:44 -0200

    Prepare to push

---
+++ ConfigFileMergerCsharp/Program.cs
@@ -0,0 +1,68 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ConfigMerge
+{
+    class Program
+    {
+        public class Options
+        {
+            [Value(0, Required = true, MetaName = "source", HelpText = "The source config file.")]
+
+            public string SourcePath { get; set; }
+            [Value(1, Required = true, MetaName = "target", HelpText = "The target config file.")]
+            public string TargetPath { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(opts => RunOptionsAndReturnExitCode(opts))
+                .WithNotParsed((errs) => HandleParseError(errs))
+                ;
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errs)
+        {
+        }
+
+        private static void RunOptionsAndReturnExitCode(Options opts)
+        {
+            var sourcepath = Path.GetFullPath(opts.SourcePath, Directory.GetCurrentDirectory());
+            var targetpath = Path.GetFullPath(opts.TargetPath, Directory.GetCurrentDirectory());
+
+            var xml = XDocument.Load(sourcepath);
+
+            var baseDir = Path.GetDirectoryName(sourcepath);
+            Change(baseDir, xml.Root);
+
+            File.WriteAllText(targetpath, xml.ToString());
+        }
+
+        private static void Change(string baseDir, XElement element)
+        {
+            var configSource = element.Attribute("configSource")?.Value;
+            if (configSource != null)
+            {
+                var configSourceFullPath = Path.Combine(baseDir, configSource);
+                if (File.Exists(configSourceFullPath))
+                {
+                    var replacement = XDocument.Load(configSourceFullPath).Root;
+                    var dirPath = Path.GetDirectoryName(baseDir);
+                    Change(dirPath, replacement);
+                    element.ReplaceWith(replacement);
+                }   
+                return;
+            }
+
+            foreach (var subElement in element.Elements().ToList())
+            {
+                Change(baseDir, subElement);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-"# ConfigFileMerger" 
+ConfigFileMerger 
+====================================
+
+[![License: MIT](https://img.shields.io/badge/license-MIT-green.svg)](LICENSE.md)
+Tool for merge `.config` files.
+
+
+How `configSource` works
+---------------------------
+Quote from Microsoft documentation article [Connection Strings and Configuration Files](https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/connection-strings-and-configuration-files#using-external-configuration-files):
+> In the main application configuration file, you use the `configSource` attribute to specify the fully qualified name and location of the external file. This example refers to an external configuration file named **connections.config**.
+> ```xml
+> <?xml version='1.0' encoding='utf-8'?>  
+> <configuration>  
+>     <connectionStrings configSource="connections.config"/>  
+> </configuration>  
+> ```
